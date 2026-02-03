60dd7f8ac8ed154e4b5d659d373ce085cae4beb1
AlvesKZ <igoralvex01@gmail.com>
2026-02-03 13:32:03 -0300

    feat: create method to find upcoming events in event service

--- src/main/java/com/eventapi/event_api/services/EventService.java
+++ src/main/java/com/eventapi/event_api/services/EventService.java
@@ -3,9 +3,13 @@ package com.eventapi.event_api.services;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventapi.event_api.domain.event.Event;
 import com.eventapi.event_api.domain.event.EventRequestDTO;
+import com.eventapi.event_api.domain.event.EventResponseDTO;
 import com.eventapi.event_api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +17,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -68,5 +73,20 @@ public class EventService {
         fos.write(multipartFile.getBytes());
         fos.close();
         return convFile;
+    }
+
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
+        return eventsPage.map(event -> new EventResponseDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getDate(),
+                "", "",
+                event.getRemote(),
+                event.getEventUrl(),
+                event.getImgUrl()
+                )).stream().toList();
     }
 }
